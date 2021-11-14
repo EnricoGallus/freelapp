99a82ef258c50f409cdcc822fc0308d8144d3511
Enrico Gallus <enrico.gallus@gmail.com>
2021-11-14 11:53:59 +0900

    fix having code available in prod image

--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -4,9 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-bundle exec rails assets:precompile
-bundle exec rails db:create
-bundle exec rails db:migrate
+rake assets:precompile
+rake db:create
+rake db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
