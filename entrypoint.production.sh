3071472e18c11813410da0c9111dbbb85e78df1a
Enrico Gallus <enrico.gallus@gmail.com>
2021-11-14 11:47:29 +0900

    fixing issues with calling rake

--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -4,9 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
-rake assets:precompile
-rake db:create
-rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails db:create
+bundle exec rails db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
