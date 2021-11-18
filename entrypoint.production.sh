09bd252c0987a36305a00764ab7b8c327dbb63a4
Enrico Gallus <enrico.gallus@gmail.com>
2021-11-18 20:28:41 +0900

    reset db

--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -3,6 +3,6 @@
 cd /app
 RAILS_ENV=production bin/rails db:create
 RAILS_ENV=production bin/rails db:migrate
-RAILS_ENV=production bin/rails db:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 RAILS_ENV=production bin/rails db:reset
 RAILS_ENV=production bin/rails assets:precompile
 bundle exec foreman start
